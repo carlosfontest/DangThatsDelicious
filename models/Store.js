387c0c1f9401f6192c9402468be48007799c33d0
Carlos Fontes <fontessystems@gmail.com>
2018-12-11 00:38:18 -0400

    se añade la opción de ingresar localicación de los stores

--- models/Store.js
+++ models/Store.js
@@ -14,15 +14,34 @@ const storeSchema = new mongoose.Schema({
     trim: true
   },
   tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  }
+
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', function (next) {
   if (!this.isModified('name')) {
     next(); // skip
     return; // stop function
   }
   this.slug = slug(this.name);
-  next(); 
+  next();
   // TODO make more resiliant so slugs are unique
 });
 
