dfd4d838d48d21d5c7d8b444b2418dc572efc9a0
Carlos Fontes <fontessystems@gmail.com>
2018-12-16 19:39:47 -0400

    se implementa el poder seleccionar una búsqueda con las teclas arriba, abajo y enter. Se hace un sanitize para que no se puedan ejecutar scripts puestos en los nombres de las stores

--- public/javascripts/modules/typeAhead.js
+++ public/javascripts/modules/typeAhead.js
@@ -1,4 +1,7 @@
-const axios = require('axios');
+import axios from 'axios';
+// Sirve para que si en algún momento una persona guarda como nombre un onload, ahi se va a ejecutar un
+// script, entonces no se ejecuta.
+import dompurify from 'dompurify';
 
 function searchResultsHTML(stores) {
   return stores.map(store => {
@@ -24,19 +27,54 @@ function typeAhead(search) {
     }
     // Mostramos los resultados
     searchResults.style.display = 'block';
-    searchResults.innerHTML = '';
 
     // Axios permite obtener ell json
     axios.get(`/api/search?q=${this.value}`)
       .then(res => {
         if (res.data.length) {
           const html = searchResultsHTML(res.data);
-          searchResults.innerHTML = html;
+          searchResults.innerHTML = dompurify.sanitize(html);
+        } else {
+          // No hay ningun resultado
+          searchResults.innerHTML = dompurify.sanitize(`<div class="search__result">No results for ${this.value} found!</div>`);
         }
       })
       .catch(error => {
         console.error(error);
       });
+  });
+
+  // Handle keyboards inputs
+  searchInput.on('keyup', (e) => {
+    // Si no se presiona arriba, abajo o enter
+    if (![38, 40, 13].includes(e.keyCode)) {
+      return;
+    }
+    const activeClass = 'search__result--active';
+    const current = search.querySelector(`.${activeClass}`);
+    const items = search.querySelectorAll('.search__result');
+    let next;
+
+    // Si pisa abajo y hay un current
+    if (e.keyCode === 40 && current) {
+      // El próximo o si no hay próximo (está en el último) será el primero
+      next = current.nextElementSibling || items[0];
+    } else if (e.keyCode === 40) {
+      next = items[0];
+    } else if (e.keyCode === 38 && current) {
+      next = current.previousElementSibling || items[items.length - 1];
+    } else if (e.keyCode === 38) {
+      next = items[items.length - 1];
+    } else if (e.keyCode === 13 && current.href) {
+      window.location = current.href;
+      return;
+    }
+
+    if (current) {
+      current.classList.remove(activeClass);
+    }
+    next.classList.add(activeClass);
+
   });
 };
 
